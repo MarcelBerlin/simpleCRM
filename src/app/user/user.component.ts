545f4fed46aece231397903f0b420133686bdca3
Marcel Berlin <berlin_m@gmx.de>
2023-06-27 19:22:42 +0200

    updated firebase subscribe() and email integration

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { MatDialog } from '@angular/material/dialog';
 import { User } from 'src/models/user.class';
-
-
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -14,18 +14,25 @@ import { User } from 'src/models/user.class';
 
 export class UserComponent implements OnInit {
 
-  user = new User(); 
+  user = new User();
+  items$: Observable<any[]>; 
+  allUsers = [];
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private firestore: Firestore) { }
 
   ngOnInit(): void {
+    const aCollection = collection(this.firestore, 'users');
+    this.items$ = collectionData(aCollection);
+
+    this.items$.subscribe(data => {
+      this.allUsers = data;
+      console.log('Received data:', data);
+    });
 
   }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent)
   }
-
-
 
 }
